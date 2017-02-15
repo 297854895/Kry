613555981e76a135b59fd91ef663d2209cf888b8
= <=>
2017-02-15 10:55:40 +0800

    update

--- src/Client.js
+++ src/Client.js
@@ -1,28 +1,28 @@
-/**
- * THIS IS THE ENTRY POINT FOR THE CLIENT, JUST LIKE server.js IS THE ENTRY POINT FOR THE SERVER.
- */
 import 'babel-polyfill';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import createStore from './redux/create';
-// import ApiClient from './helpers/ApiClient';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-// import { ReduxAsyncConnect } from 'redux-async-connect';
 import useScroll from 'scroll-behavior/lib/useStandardScroll';
 
 import getRoutes from './routes';
 import {fromJS} from 'immutable';
-// const client = new ApiClient();
+
 const history = useScroll(() => browserHistory)();
-const dest = document.getElementById('content');
 const store = createStore(history, fromJS(window.__data));
 
 const Router_ = <Router routes={getRoutes()} history={history} />;
 
-ReactDOM.render(
-  <Provider store={store} key="provider">
-    {Router_}
-  </Provider>,
-  dest
-);
+export default class Client extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Provider store={store} key="provider">
+        {Router_}
+      </Provider>
+    );
+  }
+}
