9e9f2f5beb9d87e26b477a081edd7da00b8ecc95
Kecheng Zhu <kecheng.zhu@socialcredits.cn>
2017-02-20 10:31:17 +0800

    update

--- src/redux/actions/music.js
+++ src/redux/actions/music.js
@@ -5,42 +5,76 @@ import jsonpFallback from 'jsonp-fallback';
 
 export function searchByKey(params) {
   return dispatch => {
-    let timerNum = 0;
-    let timer = setInterval(() => {
-      if (timerNum >= 50) {
-        clearInterval(timer);
-        dispatch({
-          type: ActionTypes.UPDATE_VALUE,
-          data: {
-            path: ['loading'],
-            value: 'timeout'
-          }
-        });
-        timer = false;
-        return;
-      }
-      timerNum += 1;
-    }, 100);
+    // let timerNum = 0;
+    // let timer = setInterval(() => {
+    //   if (timerNum >= 50) {
+    //     clearInterval(timer);
+    //     dispatch({
+    //       type: ActionTypes.UPDATE_VALUE,
+    //       data: {
+    //         path: ['loading'],
+    //         value: 'timeout'
+    //       }
+    //     });
+    //     timer = false;
+    //     return;
+    //   }
+    //   timerNum += 1;
+    // }, 100);
+    //
+    // jsonpFallback("http://s.music.163.com/search/get", params)
+    // .then(data => {
+    //   if (data.code === 200) {
+    //     if (timer) {
+    //       clearInterval(timer);
+    //     } else {return;}
+    //     dispatch({
+    //       type: ActionTypes.UPDATE_VALUE,
+    //       data: {
+    //         path: ['loading'],
+    //         value: false
+    //       }
+    //     });
+    //     dispatch({
+    //       type: ActionTypes.MUSIC_SEARCH,
+    //       data: data.result ? data.result : {songCount: 0, songs: []}
+    //     });
+    //   }
+    // }).catch(error => {
+    //   dispatch({
+    //     type: ActionTypes.UPDATE_VALUE,
+    //     data: {
+    //       path: ['loading'],
+    //       value: false
+    //     }
+    //   });
+    // });
 
-    jsonpFallback("http://s.music.163.com/search/get", params)
-    .then(data => {
-      if (data.code === 200) {
-        if (timer) {
+    const TIMEOUT = new Promise((reslove, reject) => {
+      let timerNum = 0;
+      const timer = setInterval(() => {
+        if (timerNum >= 50) {
+          timerNum = 0;
+          reject('timeout');
           clearInterval(timer);
-        } else {return;}
-        dispatch({
-          type: ActionTypes.UPDATE_VALUE,
-          data: {
-            path: ['loading'],
-            value: false
-          }
-        });
-        dispatch({
-          type: ActionTypes.MUSIC_SEARCH,
-          data: data.result ? data.result : {songCount: 0, songs: []}
-        });
-      }
-    }).catch(error => {
+          return;
+        }
+        timerNum += 1;
+      }, 100);
+      jsonpFallback("http://s.music.163.com/search/get", params)
+      .then(data => {
+        if (data.code === 200) {
+          timerNum = 0;
+          clearInterval(timer);
+          reslove(data);
+        }
+      }).catch(error => {
+        reject(error);
+      });
+    });
+
+    TIMEOUT.then((data) => {
+      //getsuccess
       dispatch({
         type: ActionTypes.UPDATE_VALUE,
         data: {
@@ -48,7 +82,30 @@ export function searchByKey(params) {
           value: false
         }
       });
+      dispatch({
+        type: ActionTypes.MUSIC_SEARCH,
+        data: data.result ? data.result : {songCount: 0, songs: []}
+      });
+    }, (type) => {
+      //handle error
+      if (type === 'timeout') {
+        dispatch({
+          type: ActionTypes.UPDATE_VALUE,
+          data: {
+            path: ['loading'],
+            value: 'timeout'
+          }
+        });
+      } else {
+        console.log(type);
+        dispatch({
+          type: ActionTypes.UPDATE_VALUE,
+          data: {
+            path: ['loading'],
+            value: 'false'
+          }
+        });
+      }
     });
-
   };
 }
