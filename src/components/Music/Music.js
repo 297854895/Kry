8175f57360d196627ea83a17d791691de2072f55
= <=>
2017-02-21 08:34:52 +0800

    add music

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import styles from './Music.less';
 import LoadingW from '../LoadingW/LoadingW';
+import Notification from '../Notification/Notification.js';
+let progressTimer;
 
 export default class Music extends Component {
   constructor(props) {
@@ -17,8 +19,18 @@ export default class Music extends Component {
         }
       },
       curTime: '',
-      duration: 0
+      duration: 0,
+      playListData: []
     }
+  }
+  componentDidMount() {
+    let playList = localStorage.getItem('MusicPlayList') ? JSON.parse(localStorage.getItem('MusicPlayList')) : [];
+    this.setState({
+      curMus: this.state.curMus,
+      curTime: '',
+      duration: 0,
+      playListData: playList
+    });
   }
   showMusic = () => {
     if (this.props.music.get('musicShow')) {
@@ -49,17 +61,17 @@ export default class Music extends Component {
     });
   }
   search = () => {
-    this.props.commonBoundAC.UpdateValue('UPDATE_VALUE', {
-      path: ['loading'],
-      value: true
-    });
     const key = this.refs.searchInput.value;
     if (key) {
+      this.props.commonBoundAC.UpdateValue('UPDATE_VALUE', {
+        path: ['loading'],
+        value: true
+      });
       this.props.musicBoundAc.searchByKey(
         {
           type: 1,
           s: key,
-          limit: 50
+          limit: 200
         }
       );
     }
@@ -83,7 +95,7 @@ export default class Music extends Component {
         concatChild.push('搜索');
         concatChild.push(
           <div className={styles.musicSearchWrap} key="musicSearchWrap">
-            <input ref="searchInput" placeholder="请输入歌曲名称..." onKeyDown={this.enterSearch} /><i key="music-search-input" className="fa fa-search" title="搜索" onClick={this.search}></i>
+            <input ref="searchInput" placeholder="请输入歌曲名..." onKeyDown={this.enterSearch} /><i key="music-search-input" className="fa fa-search" title="搜索" onClick={this.search}></i>
           </div>);
         break;
       case 'recommendList':
@@ -116,10 +128,9 @@ export default class Music extends Component {
     return (<ul><LoadingW></LoadingW></ul>);
   }
   searchList = (loadingStatus, data) => {
-    // console.log('xxxxxxxxxxxxxxxxxxxxxxx', loadingStatus, data);
     const output = [];
     if (loadingStatus === 'timeout') {
-      return <div key="timeout-searchlist" className={styles.noInfo}>请求超时啦...</div>;
+      return <div key="timeout-searchlist" className={styles.noInfo}>请求超时啦，少侠请从新来过...</div>;
     }
     if (!data) {
       return <ul>{loadingStatus ? <LoadingW /> : ''}</ul>;
@@ -127,7 +138,7 @@ export default class Music extends Component {
     if (data.songCount === 0 ) {
       output.push(
         <div key="noInfo-searchlist" className={styles.noInfo}>根本找不到少侠所输入的歌曲...</div>
-      )
+      );
     } else {
       data.songs.forEach((item, idx) => {
         output.push(<li className={styles.musicPlayListItem} key={'searchList' + idx + item.id}>
@@ -135,29 +146,26 @@ export default class Music extends Component {
           <span className={styles.musicName}>{item.name}</span>
           <span className={styles.song}>{item.artists[0].name}</span>
           <span className={styles.handle}>
-            <i className="fa fa-play" title="播放" onClick={this.playMusic.bind(this, item)}></i>
-            <i className="fa fa-heart" title="加入播放列表"></i>
+            <i className="fa fa-play" title="播放" onClick={this.playMusic.bind(this, {data: item, idx: 'searchPlay'})}></i>
+            <i className="fa fa-plus" title="加入播放列表" onClick={this.addPlayList.bind(this, item)}></i>
           </span>
         </li>);
       });
     }
-
     return (<ul>{loadingStatus ? <LoadingW /> : output}</ul>);
   }
   playList = (loadingStatus, data) => {
-    const musiclistdata = [
-      {name: '东风破', song: '周杰伦'},
-    ]
+    const musiclistdata = this.state.playListData ? this.state.playListData : [];
     const PlayList = [];
     musiclistdata.forEach((item, idx) => {
       PlayList.push(
         <li className={styles.musicPlayListItem} key={idx + 'music-play-list'}>
           <span className={styles.musicNum}>{idx < 9 ? '0'+ (1 + idx) : idx + 1}</span>
           <span className={styles.musicName}>{item.name}</span>
-          <span className={styles.song}>{item.song}</span>
+          <span className={styles.song}>{item.artists[0].name}</span>
           <span className={styles.handle}>
-            <i className="fa fa-play" title="播放"></i>
-            <i className="fa fa-trash" title="删除"></i>
+            <i className="fa fa-play" title="播放" onClick={this.playMusic.bind(this, {data: item, idx: idx})}></i>
+            <i className="fa fa-trash" title="删除" onClick={this.removePlayList.bind(this, item)}></i>
           </span>
         </li>
       );
@@ -166,7 +174,11 @@ export default class Music extends Component {
   }
   handleCurMusic = (play) => {
     if (!this.state.curMus.audio) {
-      console.log('没有歌曲');
+      Notification({
+        type: 'info',
+        context: '没有歌曲如何播放...',
+        close: 2000
+      });
       return;
     }
     if (play) {
@@ -185,19 +197,44 @@ export default class Music extends Component {
     });
   }
   toggleMusic = (action) => {
-    console.log(action);
+    let curIdx = this.refs.player.getAttribute('data-listIdx');
+    const playListData_ = this.state.playListData;
+    let newIdx;
+    if (curIdx === 'newList') {
+      if (playListData_[0]) {
+        this.playMusic({data: playListData_[0], idx: 0});
+        return;
+      }
+      Notification({
+        type: 'info',
+        context: '没有歌曲如何播放...',
+        close: 2000
+      });
+      return;
+    }
+    curIdx = parseInt(curIdx, 10);
+    if (action === 'next') {
+      newIdx = curIdx + 1 >= playListData_.length ? 0 : curIdx + 1;
+    } else {
+      newIdx = curIdx - 1 < 0 ? playListData_.length - 1 : curIdx - 1;
+    }
+    this.playMusic({data: playListData_[newIdx], idx: newIdx});
+    return;
   }
   playMusic = (data) => {
     const oldSrc = this.refs.player.src;
-    const thisSong = oldSrc === data.audio ? true : false;
-    if (this.state.progressTimer) {
-      clearInterval(this.state.progressTimer);
+    const thisSong = oldSrc === data.data.audio ? true : false;
+    if (data.idx !== 'searchPlay') {
+      this.refs.player.setAttribute('data-listIdx', data.idx);
+    }
+    if (progressTimer) {
+      clearInterval(progressTimer);
     }
     this.setState({
-      curMus: data,
+      curMus: data.data,
       curTime: '',
       duration: 0,
-      progressTimer: ''
+      playListData: this.state.playListData
     });
     if (!thisSong) {
       const playTimer = setInterval(() => {
@@ -214,33 +251,119 @@ export default class Music extends Component {
       this.playerTimeProgress();
     }
   }
+  addPlayList = (item) => {
+    let playListData = localStorage.getItem('MusicPlayList') ? JSON.parse(localStorage.getItem('MusicPlayList')) : [];
+    if (playListData.length === 0) {
+      const listData = [item];
+      this.setState({
+        curMus: this.state.curMus,
+        curTime: this.refs.player.currentTime,
+        duration: this.refs.player.duration,
+        playListData: listData
+      });
+      localStorage.setItem('MusicPlayList', JSON.stringify(listData));
+      Notification({
+        type: 'success',
+        context: `添加歌曲《${item.name}》 成功！ `,
+        close: 2000
+      });
+      return;
+    }
+    try {
+      playListData.forEach((item_) => {
+        if (item_.id === item.id) {
+          throw new Error ('already exists');
+        }
+      });
+    } catch (e) {
+      // 已经存在
+      Notification({
+        type: 'info',
+        context: '播放列表中已经存在这首歌啦...',
+        close: 2000
+      });
+      return;
+    }
+    const newList = playListData;
+    newList.push(item);
+    this.setState({
+      curMus: this.state.curMus,
+      curTime: this.refs.player.currentTime,
+      duration: this.refs.player.duration,
+      playListData: newList
+    });
+    localStorage.setItem('MusicPlayList', JSON.stringify(newList));
+    Notification({
+      type: 'success',
+      context: `添加歌曲《${item.name}》 成功！ `,
+      close: 2000
+    });
+  }
+  removePlayList = (item) => {
+    let playListData = localStorage.getItem('MusicPlayList') ? JSON.parse(localStorage.getItem('MusicPlayList')) : [];
+    if (playListData.length === 0 || playListData.length === 1) {
+      this.setState({
+        curMus: this.state.curMus,
+        curTime: this.refs.player.currentTime,
+        duration: this.refs.player.duration,
+        playListData: []
+      });
+      localStorage.setItem('MusicPlayList', '');
+      Notification({
+        type: 'success',
+        context: `移除歌曲《${item.name}》 成功！ `,
+        close: 2000
+      });
+      return;
+    }
+    try {
+      playListData.forEach((item_, idx) => {
+        if (item_.id === item.id) {
+          throw new Error (idx);
+        }
+      });
+    } catch (e) {
+      const idx = parseInt(e.toString().substring(7), 10);
+      const arrLeft = playListData.slice(0, idx);
+      const arrRight = playListData.slice(idx + 1, playListData.length);
+      this.setState({
+        curMus: this.state.curMus,
+        curTime: this.refs.player.currentTime,
+        duration: this.refs.player.duration,
+        playListData: arrLeft.concat(arrRight)
+      });
+      localStorage.setItem('MusicPlayList', JSON.stringify(arrLeft.concat(arrRight)));
+      Notification({
+        type: 'success',
+        context: `移除歌曲《${item.name}》 成功！ `,
+        close: 2000
+      });
+      return;
+    }
+  }
   playerTimeProgress = () => {
     //设置播放状态为true
     this.props.commonBoundAC.UpdateValue('UPDATE_VALUE', {
       path: ['play'],
       value: true
     });
-    this.setState({
-      curMus: this.state.curMus,
-      curTime: this.refs.player.currentTime,
-      duration: this.refs.player.duration,
-      progressTimer: setInterval(() => {
-        if (this.refs.player.currentTime === this.refs.player.duration) {
-          //播放完成
-          this.props.commonBoundAC.UpdateValue('UPDATE_VALUE', {
-            path: ['play'],
-            value: false
-          });
-          clearInterval(progressTimer);
-        }
-        this.setState({
-          curMus: this.state.curMus,
-          curTime: this.refs.player.currentTime,
-          duration: this.refs.player.duration,
-          progressTimer: this.state.progressTimer
+    progressTimer = setInterval(() => {
+      if (this.refs.player.currentTime === this.refs.player.duration) {
+        //播放完成
+        this.props.commonBoundAC.UpdateValue('UPDATE_VALUE', {
+          path: ['play'],
+          value: false
         });
-      }, 1000)
-    });
+        clearInterval(progressTimer);
+        progressTimer = false;
+      }
+      this.setState({
+        curMus: this.state.curMus,
+        curTime: this.refs.player.currentTime,
+        duration: this.refs.player.duration,
+        playListData: this.state.playListData
+      });
+    }, 1000);
   }
   timeToDoublue = (time) => {
     let min = parseInt(time / 60, 10);
@@ -260,7 +383,7 @@ export default class Music extends Component {
     const searchListData = this.props.music.get('searchList') ? this.props.music.get('searchList').toJS() : this.props.music.get('searchList');
     return (
       <div key="Music" className={styles.Music} style={{left: musicShow ? '0' : '-550px'}}>
-        <audio style={{display: 'none'}} ref="player" src={this.state.curMus.audio ? this.state.curMus.audio : 'nosrc'}></audio>
+        <audio style={{display: 'none'}} data-listIdx='newList' ref="player" src={this.state.curMus.audio ? this.state.curMus.audio : 'nosrc'}></audio>
         <div className={styles.musicPane}>
           <div className={styles.musicImg}>
             {this.state.curMus.album.picUrl ? <img src={this.state.curMus.album.picUrl} /> : <div><span></span></div>}
--- src/redux/actions/music.js
+++ src/redux/actions/music.js
@@ -5,51 +5,6 @@ import jsonpFallback from 'jsonp-fallback';
 
 export function searchByKey(params) {
   return dispatch => {
-    // let timerNum = 0;
-    // let timer = setInterval(() => {
-    //   if (timerNum >= 50) {
-    //     clearInterval(timer);
-    //     dispatch({
-    //       type: ActionTypes.UPDATE_VALUE,
-    //       data: {
-    //         path: ['loading'],
-    //         value: 'timeout'
-    //       }
-    //     });
-    //     timer = false;
-    //     return;
-    //   }
-    //   timerNum += 1;
-    // }, 100);
-    //
-    // jsonpFallback("http://s.music.163.com/search/get", params)
-    // .then(data => {
-    //   if (data.code === 200) {
-    //     if (timer) {
-    //       clearInterval(timer);
-    //     } else {return;}
-    //     dispatch({
-    //       type: ActionTypes.UPDATE_VALUE,
-    //       data: {
-    //         path: ['loading'],
-    //         value: false
-    //       }
-    //     });
-    //     dispatch({
-    //       type: ActionTypes.MUSIC_SEARCH,
-    //       data: data.result ? data.result : {songCount: 0, songs: []}
-    //     });
-    //   }
-    // }).catch(error => {
-    //   dispatch({
-    //     type: ActionTypes.UPDATE_VALUE,
-    //     data: {
-    //       path: ['loading'],
-    //       value: false
-    //     }
-    //   });
-    // });
-
     const TIMEOUT = new Promise((reslove, reject) => {
       let timerNum = 0;
       const timer = setInterval(() => {
@@ -74,7 +29,6 @@ export function searchByKey(params) {
         reject(error);
       });
     });
-
     TIMEOUT.then((data) => {
       //getsuccess
       dispatch({
